8263e7c47b447b6fcf30cb4388a850e9b9f01c8e
Nickley <83526077+nickl3y@users.noreply.github.com>
2023-12-31 04:31:22 +0500

    a

---
+++ AtomVR/Plugins/AssetsCleaner/Intermediate/Build/Mac/UnrealEditor/Inc/AssetsCleaner/UHT/AssetsCleanerSettings.generated.h
@@ -0,0 +1,68 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "AssetsCleanerSettings.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ASSETSCLEANER_AssetsCleanerSettings_generated_h
+#error "AssetsCleanerSettings.generated.h already included, missing '#pragma once' in AssetsCleanerSettings.h"
+#endif
+#define ASSETSCLEANER_AssetsCleanerSettings_generated_h
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_SPARSE_DATA
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAssetsCleanerSettings(); \
+	friend struct Z_Construct_UClass_UAssetsCleanerSettings_Statics; \
+public: \
+	DECLARE_CLASS(UAssetsCleanerSettings, UObject, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AssetsCleaner"), NO_API) \
+	DECLARE_SERIALIZER(UAssetsCleanerSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("EditorPerProjectUserSettings");} \
+
+
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAssetsCleanerSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAssetsCleanerSettings(UAssetsCleanerSettings&&); \
+	NO_API UAssetsCleanerSettings(const UAssetsCleanerSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAssetsCleanerSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAssetsCleanerSettings); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAssetsCleanerSettings) \
+	NO_API virtual ~UAssetsCleanerSettings();
+
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_11_PROLOG
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_SPARSE_DATA \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_INCLASS_NO_PURE_DECLS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ASSETSCLEANER_API UClass* StaticClass<class UAssetsCleanerSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_AssetsCleaner_Source_AssetsCleaner_Public_AssetsCleanerSettings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
