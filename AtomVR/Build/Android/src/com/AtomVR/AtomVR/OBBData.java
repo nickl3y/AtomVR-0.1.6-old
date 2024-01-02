ed86f69439b6840418317ff8395395a6c4a4e60a
Nickley <83526077+nickl3y@users.noreply.github.com>
2024-01-02 18:50:43 +0500

    ///

--- AtomVR/Build/Android/src/com/AtomVR/AtomVR/OBBData.java
+++ AtomVR/Build/Android/src/com/AtomVR/AtomVR/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-549662696L // the length of the file in bytes
+549662355L // the length of the file in bytes
 )
 };
 };
