1b34d0d62f1d9e56c8e2dd1a9a2a93ba158c94a9
Nickley <83526077+nickl3y@users.noreply.github.com>
2024-01-25 18:09:45 +0500

    kughi

--- AtomVR/Build/Android/src/com/CWE/AtomVR/OBBData.java
+++ AtomVR/Build/Android/src/com/CWE/AtomVR/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-625760710L // the length of the file in bytes
+675787926L // the length of the file in bytes
 )
 };
 };
